a2f9eb86d2d73b83a5dafc2849124481bda8aed7
Nikolay Sosin <nmsosin@yandex.ru>
2023-11-08 07:50:48 +0300

    Add sorting feature

--- src/pages/task-list/task-list.tsx
+++ src/pages/task-list/task-list.tsx
@@ -1,18 +1,57 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { TStore } from '../../services/reducers';
 import styles from './styles.module.css';
 import { TaskItem } from 'app/task-item/task-item';
 import { TTask } from 'types/tasks';
-import { SortBar } from 'app/sort-bar/sort-bar';
 
 export const TaskList: FC = () => {
+  const [sort, setSort] = useState<'all' | 'active' | 'done' | 'important'>('all');
   const tasks = useSelector((store: TStore) => store.taskList.tasks);
-  console.log(tasks);
+  const filteredTasks = tasks.filter((task) => {
+    switch (sort) {
+      case 'active':
+        return !task.isCompleted;
+      case 'done':
+        return task.isCompleted;
+      case 'important':
+        return task.isImportant;
+      default:
+        return task;
+    }
+  });
+  console.log(filteredTasks);
   return (
     <section>
-      <SortBar />
-      <ul className={styles.tasks}>{tasks && tasks.map((task: TTask) => <TaskItem task={task} key={task._id} />)}</ul>
+      <nav className={styles.sortBar}>
+        <button
+          onClick={() => setSort('all')}
+          className={`${styles.sortButton} ${sort === 'all' ? styles.active : ''}`}
+          type={'button'}>
+          All
+        </button>
+        <button
+          onClick={() => setSort('active')}
+          className={`${styles.sortButton} ${sort === 'active' ? styles.active : ''}`}
+          type={'button'}>
+          Active
+        </button>
+        <button
+          onClick={() => setSort('done')}
+          className={`${styles.sortButton} ${sort === 'done' ? styles.active : ''}`}
+          type={'button'}>
+          Done
+        </button>
+        <button
+          onClick={() => setSort('important')}
+          className={`${styles.sortButton} ${sort === 'important' ? styles.active : ''}`}
+          type={'button'}>
+          Important
+        </button>
+      </nav>
+      <ul className={styles.tasks}>
+        {filteredTasks && filteredTasks.map((task: TTask) => <TaskItem task={task} key={task._id} />)}
+      </ul>
     </section>
   );
 };
